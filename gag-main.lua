de73e3e609ecfd52641a8c294a21ca6391097b77
tuanphong014 <tuanphong014@gmail.com>
2025-08-10 18:50:05 +0700

    Update gag-main.lua

--- gag-main.lua
+++ gag-main.lua
@@ -15,7 +15,7 @@ getgenv().gagConfig = {
     ADD_FRIEND = true,
     OPEN_ALL_SEED_PACK = true,
 	FORCE_COOK_MUTATION_ONLY = true,
-	PLACE_COSMETIC = { "Kitchen Cart", "Smoothie Fountain", "Kitchen Flooring", "Cooking Cauldron", "Kitchen Crate", "Gourmet Seed Pack" }
+	PLACE_COSMETIC = { "Cooking Cauldron", "Kitchen Crate", "Kitchen Flooring", "Kitchen Cart", "Smoothie Fountain", "Gourmet Seed Pack" }
 
 
     MAX_PLANTS = 250,
